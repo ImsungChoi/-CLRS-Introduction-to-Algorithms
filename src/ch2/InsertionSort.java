38a7520970f413158992ada1a7b026f9cc14e6e5
Imsung Choi <imsung.choi@sk.com>
2015-08-28 13:06:57 +0900

    First Release
    First Release

---
+++ src/ch2/InsertionSort.java
@@ -0,0 +1,42 @@
+package ch2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int c = sc.nextInt();
+
+        while (c-- > 0) {
+            int size = sc.nextInt();
+            List<Integer> input = new ArrayList<Integer>();
+
+            for (int i = 0; i < size; i++) {
+                input.add(sc.nextInt());
+            }
+
+            System.out.println(insertionSort(input));
+        }
+
+        sc.close();
+    }
+
+    public static List<Integer> insertionSort(List<Integer> intList) {
+        for (int i = 1; i < intList.size(); i++) {
+            int key = intList.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && intList.get(j) > key) {
+                intList.set(j + 1, intList.get(j));
+                j = j - 1;
+            }
+            intList.set(j + 1, key);
+        }
+
+        return intList;
+    }
+}
---
+++ src/ch2/SelectionSort.java
@@ -0,0 +1,41 @@
+package ch2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+
+        while (c-- > 0) {
+            int n = sc.nextInt();
+            List<Integer> input = new ArrayList<Integer>();
+
+            for (int i = 0; i < n; i++)
+                input.add(sc.nextInt());
+
+            System.out.println(selectionSort(input));
+        }
+        sc.close();
+    }
+
+    public static List<Integer> selectionSort(List<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            int min = i;
+
+            for (int j = min + 1; j < list.size(); j++) {
+                if (list.get(min) > list.get(j)) {
+                    min = j;
+                }
+            }
+
+            int temp = list.get(min);
+            list.set(min, list.get(i));
+            list.set(i, temp);
+        }
+        return list;
+    }
+}
